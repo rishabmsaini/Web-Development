e6af9dadcff578e103df824714d37ae1d6ce2c07
Rishab Saini <rishabmsaini@gmail.com>
2019-10-28 17:07:25 +0530

    Create input.php

---
+++ Prime/input.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Prime Numbers</title>
+</head>
+<body>
+	<form action="prime.php" method="post">
+		Number 1: <input type="number" name="input1"><br>
+		Number 2: <input type="number" name="input2"><br>
+		<input type="submit">
+	</form>
+</body>
+</html>
