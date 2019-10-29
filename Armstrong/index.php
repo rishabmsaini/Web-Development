f9808c64da80dd85c0c4b41b556c40b880035e4b
Rishab Saini <rishabmsaini@gmail.com>
2019-10-29 14:02:40 +0530

    Create index.php

---
+++ Armstrong/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Armstrong Number</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="container">
+		<div class="page-header">
+	  		<h1>Armstrong Number <small>Enter any integer!</small></h1>
+		</div>
+		<div class="jumbotron">
+			<form class="navbar-form navbar-left" action="arm.php" method="post">
+				<div class="form-group">
+					<p class="navbar-text shift">INPUT:
+					<input type="number" class="form-control" placeholder="Enter a number" name="input">
+					</p>
+				</div>
+				<br><br><br>
+				<input class="btn btn-success btn-lg btn-block active" type="submit" value="submit">
+			</form>
+		</div>
+	</div>
+</body>
+</html>	
